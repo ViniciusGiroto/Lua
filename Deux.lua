7852b78e7d29b90acbcade30605bb124d1061093
Squalleze <vinnicius_giroto@outlook.com>
2014-11-16 15:21:55 -0200

    Update Deux.lua

--- Deux.lua
+++ Deux.lua
@@ -1,17 +1,28 @@
 Maps = {'@4685402', '@4664013', '@4659007', '@4590128', '@4590114', '@4590098', '@4590090', '@4590082', '@4434919', '@4082213', '@2969962', '@2873573', '@2462332', '@2454924', '@2449267', '@1870464', '@2396838', '@4007168', '@1124454', '@452653', '@2113338', '@2199656', '@3820670', '@159709', '@3147428', '@3051156', '@172357', '@3617459', '@413242', '@2932610', '@317521', '@155582', '@460646', '@156966', '@200017', '@489046', '@584430', '@1193841', '@187059', '@169710', '@458677', '@188821', '@308787', '@474113', '@3049560', '@1233889', '@1233889', '@249255', '@436128', '@382841', '@219032', '@1641473', '@497021', '@364544', '@504567', '@504166', '@2703778', '@2406202', '@355562', '@166138', '@426398', '@442098', '@776051', '@923878', '@1937184', '@437313', '@416324', '@528954', '@392150', '@421185', '@484159', '@470160', '@398828', '@273581', '@182878', '@4652489', '@4679376', '@4666553'}
-Shop = {Grounds = {['Wood'] = {0, 0, P = {0.3, 0.2}}, ['Sand'] = {75, 7, P = {0.1, 0.2}}, ['Earth'] = {100, 5, P = {0.3, 0.2}}, ['Grass'] = {150, 6, P = {0.3, 0.2}}, ['Stone'] = {500, 10, P = {0.3, 0}}, ['Colorful'] = {750, 12, P = {0.3, 0.2}}, ['Chocolate'] = {1000, 4, P = {20, 0.2}}}, ['Resize'] = {200, 200}}
+Shop = {Grounds = {['Web'] = {100, 15, P = {0.3, 0.2}}, ['Wather'] = {100, 9, P = {0.3, 0.2}}, ['Wood'] = {0, 0, P = {0.3, 0.2}}, ['Sand'] = {75, 7, P = {0.1, 0.2}}, ['Earth'] = {100, 5, P = {0.3, 0.2}}, ['Grass'] = {150, 6, P = {0.3, 0.2}}, ['Stone'] = {500, 10, P = {0.3, 0}}, ['Colorful'] = {750, 12, P = {0.3, 0.2}}, ['Chocolate'] = {1000, 4, P = {20, 0.2}}}, Skills = {['Resize'] = {200, 200}}}
 PDB = {}
 PS = {['Squalleze'] = 5, ['Mrorko'] = 5}
-Grounds = 0
+Ground = 0
 Sps = 0
 
 tfm.exec.disableAllShamanSkills(true)
 tfm.exec.disableAutoNewGame(true)
 system.disableChatCommandDisplay('p', true)
 system.disableChatCommandDisplay('s', true)
+system.disableChatCommandDisplay('np', true)
 
-eventShop = function(P)
-	local T = ''
+ND = function(T)
+	if string.sub(T, 1, 1) == '+' then
+		local T = string.upper(string.sub(T, 1, 2)) .. string.lower(string.sub(T, 3))
+		return T
+	else
+		local T = string.upper(string.sub(T, 1, 1)) .. string.lower(string.sub(T, 2))
+		return T
+	end
+end
+
+eventShop = function(P, C)
+	local T = '<br><br><n>Grounds:'
 	for G in pairs(Shop.Grounds) do
 		if PDB[P].Inventory[G] and PDB[P].GroundU ~= Shop.Grounds[G][2] and Shop.Grounds[G][2] ~= 12 then
 			T = T .. '<br><bl>» ' .. G .. ' : <v><a href="event:Equip_' .. G .. '">Equip</a>'
@@ -25,20 +36,36 @@ eventShop = function(P)
 			T = T .. '<br><bl>» ' .. G .. ' : <v><a href="event:Buy_' .. G .. '">Buy ' .. Shop.Grounds[G][1] .. '</a>'
 		end
 	end
-	return T
+	local K = '<br><br><n>Skills:'
+	for S in pairs(Shop.Skills) do
+		if PDB[P].Inventory[S] and not PDB[P].Skills[S] then
+			K = K .. '<br><bl>» ' .. S .. ' : <v><a href="event:Equip_' .. S .. '">Equip</a>'
+		elseif PDB[P].Skills[S] then
+			K = K .. '<br><bl>» ' .. S .. ' : <v><a href="event:Unequip_' .. S .. '">Unequip</a>'
+		elseif not PDB[P].Inventory[S] then
+			K = K .. '<br><bl>» ' .. S .. ' : <v><a href="event:Buy_' .. S .. '">Buy ' .. Shop.Skills[S][1] .. '</a>'
+		end
+	end
+	if C == 1 then
+		return T
+	elseif C == 2 then
+		return K
+	end
 end
 
 eventNewPlayer = function(P)
 	if not PDB[P] then
 		PDB[P] = {
 			ID = #PDB + 1,
-			PeL = PS[P] or 0,
+			PL = PS[P] or 0,
 			HaM = tfm.get.room.playerList[P].inHardMode,
-			Coins = PS[P] + 99999 ^ 9 or 0,
+			O = false,
+			Coins = 99999 ^ 9 or 0,
 			GroundU = 0,
 			GroundN = 'Wood',
 			Colorful = 3294800,
 			Inventory = {['Wood'] = true},
+			Skills = {},
 			Profil = {
 				Saves = {0, 0, 0},
 				Rounds = {0, 0}
@@ -51,7 +78,7 @@ end
 
 eventNewGame = function()
 	isAlive = 0
-	Grounds = 0
+	Ground = 0
 	Sps = 0
 	for _ in pairs(tfm.get.room.playerList) do
 		isAlive = isAlive + 1
@@ -92,12 +119,30 @@ eventPlayerWon = function(P)
 end
 
 eventChatCommand = function(P, C)
-	if C == 'p' then
-		ui.addTextArea(2, string.format('<font size="20">%s</font><br><br>Saves:<br><tab><bl>» Normal : <v>%s<br><tab><bl>» Hard: <j>%s<br><tab><bl>» Divine: <r>%s<br><br><n>Rounds:<br><tab><bl>» Played: <v>%s<br><tab><bl>» Completed: <v>%s', P, PDB[P].Profil.Saves[1], PDB[P].Profil.Saves[2], PDB[P].Profil.Saves[3], PDB[P].Profil.Rounds[1], PDB[P].Profil.Rounds[2]), P, 290, 100, 220, 200, 768, 768, .8, true)
-		ui.addTextArea(3, '<a href="event:pc"><p align="center"><font color="#808080" size="12">Close</font></p></a>', P, 295, 276, 210, nil, 0x808080, 0x404040, .2, true)
-	elseif C == 's' then
-		ui.addTextArea(2, string.format('<font size="20">Shop</font><br><br>Grounds:%s<br><p align="right"><n>Coins : <j>%s</a>', eventShop(P), PDB[P].Coins), P, 290, 100, 220, 200, 768, 768, .8, true)
-		ui.addTextArea(3, '<a href="event:pc"><p align="center"><font color="#808080" size="12">Close</font></p></a>', P, 295, 276, 210, nil, 0x808080, 0x404040, .2, true)
+	local R = PDB[P].PL
+	local A = {}
+
+	for V in string.gmatch(C, '[^%s]+') do
+		table.insert(A, V)
+	end
+
+	local C = string.lower(A[1])
+	if C == 'p' and not A[2] and not PDB[P].O then
+		PDB[P].O = true
+		ui.addTextArea(2, string.format('<font size="20">%s</font><br><br>Saves:<br><tab><bl>» Normal : <v>%s<br><tab><bl>» Hard: <j>%s<br><tab><bl>» Divine: <r>%s<br><br><n>Rounds:<br><tab><bl>» Played: <v>%s<br><tab><bl>» Completed: <v>%s', P, PDB[P].Profil.Saves[1], PDB[P].Profil.Saves[2], PDB[P].Profil.Saves[3], PDB[P].Profil.Rounds[1], PDB[P].Profil.Rounds[2]), P, 290, 100, 220, 202, 0x324650, 0x27373F, 1, true)
+		ui.addTextArea(3, '<a href="event:pc"><p align="center"><font size="12">Close</font></p></a>', P, 295, 278, 210, nil, 0x27373F, 0x324650, .8, true)
+	elseif C == 'p' and A[2] and PDB[ND(A[2])] and not PDB[P].O then
+		PDB[P].O = true
+		ui.addTextArea(2, string.format('<font size="20">%s</font><br><br>Saves:<br><tab><bl>» Normal : <v>%s<br><tab><bl>» Hard: <j>%s<br><tab><bl>» Divine: <r>%s<br><br><n>Rounds:<br><tab><bl>» Played: <v>%s<br><tab><bl>» Completed: <v>%s', ND(A[2]), PDB[ND(A[2])].Profil.Saves[1], PDB[ND(A[2])].Profil.Saves[2], PDB[ND(A[2])].Profil.Saves[3], PDB[ND(A[2])].Profil.Rounds[1], PDB[ND(A[2])].Profil.Rounds[2]), P, 290, 100, 220, 202, 0x324650, 0x27373F, 1, true)
+		ui.addTextArea(3, '<a href="event:pc"><p align="center"><font size="12">Close</font></p></a>', P, 295, 278, 210, nil, 0x27373F, 0x324650, .8, true)
+	elseif C == 's' and not PDB[P].O then
+		PDB[P].O = true
+		ui.addTextArea(4, '<a href="event:grounds"><font size="12">Grounds</font></a>', P, 220, 110, 105, nil, 0x324650, 0x27373F, 1, true)
+		ui.addTextArea(5, '<a href="event:skills"><font size="12">Skills</font></a>', P, 220, 140, 105, nil, 0x324650, 0x27373F, 1, true)
+		ui.addTextArea(6, string.format('<font size="20">Shop</font>%s', eventShop(P, 1)), P, 290, 100, 220, 202, 0x324650, 0x27373F, 1, true)
+		ui.addTextArea(7, '<a href="event:sc"><p align="center"><font size="12">Close</font></p></a>', P, 295, 278, 210, nil, 0x27373F, 0x324650, .8, true)
+	elseif C == 'np' and R >= 5 then
+		pcall(tfm.exec.newGame, A[2])
 	end
 end
 
@@ -105,6 +150,12 @@ eventTextAreaCallback = function(I, P, C)
 	if C == 'pc' then
 		ui.removeTextArea(2, P)
 		ui.removeTextArea(3, P)
+		PDB[P].O = false
+	elseif C == 'sc' then
+		for i = 4, 7 do
+			ui.removeTextArea(i, P)
+		end
+		PDB[P].O = false
 	elseif string.sub(C, 1, 4) == 'Buy_' then
 		G = string.sub(C, 5)
 		if Shop.Grounds[G] and not PDB[P].Inventory[G] then
@@ -121,6 +172,10 @@ eventTextAreaCallback = function(I, P, C)
 		eventChatCommand(P, 's')
 	elseif C == 'Edit' then
 		ui.addPopup(1, 2, 'Enter with ground color:<br>Example : #FADE55', P, 300, 150, 200, true)
+	elseif C == 'grounds' then
+		ui.updateTextArea(6, string.format('<n><font size="20">Shop</font>%s', eventShop(P, 1)), P)
+	elseif C == 'skills' then
+		ui.updateTextArea(6, string.format('<n><font size="20">Shop</font>%s', eventShop(P, 2)), P)
 	end
 end
 
@@ -139,13 +194,14 @@ end
 eventSummoningEnd = function(p, o, x, y, a, xs, ys, t, e)
 	if o == 0 then
 		if PDB[p].HaM < 1 then
-			Grounds = Grounds + 1
-			tfm.exec.addPhysicObject(Grounds, x, y, {type = PDB[p].GroundU, width = 100, height = 10, angle = a, friction = Shop.Grounds[PDB[p].GroundN].P[1], restitution = Shop.Grounds[PDB[p].GroundN].P[2], color = PDB[p].Colorful})
-			tfm.exec.removePhysicObject(Grounds - 2)
+			if Ground == 1 then
+				Ground = 2
+			else
+				Ground = 1
+			end
+			tfm.exec.addPhysicObject(Ground, x, y, {type = PDB[p].GroundU, width = 100, height = 10, angle = a, friction = Shop.Grounds[PDB[p].GroundN].P[1], restitution = Shop.Grounds[PDB[p].GroundN].P[2], color = PDB[p].Colorful})
 		elseif PDB[p].HaM > 0 then
-			Grounds = Grounds + 1
-			tfm.exec.addPhysicObject(Grounds, x, y, {type = PDB[p].GroundU, width = 100, height = 10, angle = a, friction = Shop.Grounds[PDB[p].GroundN].P[1], restitution = Shop.Grounds[PDB[p].GroundN].P[2], color = PDB[p].Colorful})
-			tfm.exec.removePhysicObject(Grounds - 1)
+			tfm.exec.addPhysicObject(1, x, y, {type = PDB[p].GroundU, width = 100, height = 10, angle = a, friction = Shop.Grounds[PDB[p].GroundN].P[1], restitution = Shop.Grounds[PDB[p].GroundN].P[2], color = PDB[p].Colorful})
 		else
 			print(error())
 		end
@@ -156,7 +212,7 @@ eventSummoningEnd = function(p, o, x, y, a, xs, ys, t, e)
 			tfm.exec.chatMessage('<R>Alert! You used the maximum number of spirits per round.', p);
 			tfm.exec.chatMessage('<CH>The shaman used the maximum number of spirits per round.');
 		else
-			tfm.exec.chatMessage('<J>Alert! You used a spirit. Remainder: ' .. 2 - Sps, p);
+			tfm.exec.chatMessage('<J>Alert! You used a spirit. Remainder: <v>' .. 2 - Sps, p);
 		end
 	elseif o ~= 0 and o ~= 24 then
 		tfm.exec.killPlayer(p)
