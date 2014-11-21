e0cd5ce9da0c22a77d02b6c416672929ba0dd1c9
Squalleze <vinnicius_giroto@outlook.com>
2014-11-21 00:58:15 -0200

    Create Speed 2.0.lua

---
+++ Speed 2.0.lua
@@ -0,0 +1,51 @@
+PDB = {}
+
+PD = function(P)
+	if not PDB[P] then
+		PDB[P] = {
+			P = '';
+		}
+	end
+end
+
+eventNewPlayer = function(P)
+	PD(P)
+	tfm.exec.bindKeyboard(P, 37, true, true)
+	tfm.exec.bindKeyboard(P, 37, false, true)
+	tfm.exec.bindKeyboard(P, 39, true, true)
+	tfm.exec.bindKeyboard(P, 39, false, true)
+end
+
+eventLoop = function(C, R)
+	for P in pairs(tfm.get.room.playerList) do
+		if PDB[P].P == 'Left' then
+			tfm.exec.movePlayer(P, 0, 0, false, -25, 0, true)
+			break
+		elseif PDB[P].P == 'Right' then
+			tfm.exec.movePlayer(P, 0, 0, false, 25, 0, true)
+			break
+		else
+			break
+		end
+	end
+end
+
+eventKeyboard = function(P, K, D, X, Y)
+	if K == 37 then
+		if D == true then
+			PDB[P].P = 'Left'
+		else
+			PDB[P].P = ''
+		end
+	elseif K == 39 then
+		if D == true then
+			PDB[P].P = 'Right'
+		else
+			PDB[P].P = ''
+		end
+	end
+end
+
+for P in pairs(tfm.get.room.playerList) do
+	eventNewPlayer(P)
+end
