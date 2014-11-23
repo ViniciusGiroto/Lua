821180249251d1de1aeac97d70a025b8f0d2af28
Squalleze <vinnicius_giroto@outlook.com>
2014-11-23 02:29:02 -0200

    Create XMLCollect.lua

---
+++ XMLCollect.lua
@@ -0,0 +1,12 @@
+Xml = nil
+
+eventNewGame = function()
+	Xml = tfm.get.room.xmlMapInfo.xml
+end
+
+eventChatCommand = function(P, C)
+	if type(tonumber(C)) == 'number' then
+		C = tonumber(C)
+		print(Xml:gsub('<', '&lt;'):sub(C * 2000 + 1, C * 2000 + 2000))
+	end
+end
