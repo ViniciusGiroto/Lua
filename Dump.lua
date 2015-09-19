2b96c92bfc9f44574f651055dff1e54b061bba1d
Squalleze <vinnicius_giroto@outlook.com>
2015-09-19 16:10:57 -0300

    Create Dump.lua

---
+++ Dump.lua
@@ -0,0 +1,30 @@
+local seen = {}
+
+oldprint = print
+print = function(...)
+	local _ = {}
+	table.foreach({...}, function(k, v) table.insert(_, tostring(v)) end)
+	oldprint(table.concat(_, '\t'))
+end
+
+local show = '\n'
+
+function dump(t,i)
+	seen[t] = true
+	local s = {}
+	local n = 0
+	for k in pairs(t) do
+		n = n + 1 s[n] = k
+	end
+	table.sort(s)
+	for k, v in ipairs(s) do
+		show = show .. i .. v .. '\n'
+		v = t[v]
+		if type(v) == 'table' and not seen[v] then
+			dump(v, i .. '\t')
+		end
+	end
+end
+
+dump(_G, '')
+print(show)
