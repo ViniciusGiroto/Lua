0e95338da65df3dbfb75b60c593ff5ddef748f3b
Squalleze <vinnicius_giroto@outlook.com>
2015-10-05 14:47:16 -0300

    Update Speed.lua

--- Speed.lua
+++ Speed.lua
@@ -1,10 +1,11 @@
 -- Created by Squalleze
+impulse = 25
 eventLoop = function()
 	for k, v in pairs(tfm.get.room.playerList) do
 		if v.movingLeft then
-			tfm.exec.movePlayer(k, 0, 0, false, -25, 0, true)
+			tfm.exec.movePlayer(k, 0, 0, false, - impulse, 0, true)
 		elseif v.movingRight then
-			tfm.exec.movePlayer(k, 0, 0, false, 25, 0, true)
+			tfm.exec.movePlayer(k, 0, 0, false, impulse, 0, true)
 		end
 	end
 end
