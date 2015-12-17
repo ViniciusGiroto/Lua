799cdefa510dba2887aadfc33dce367cb566e4fd
Squalleze <vinnicius_giroto@outlook.com>
2015-12-17 04:13:05 -0200

    Update VerifyDate.lua

--- VerifyDate.lua
+++ VerifyDate.lua
@@ -2,8 +2,8 @@
 verifyDate = function(day, month, year)
     local leapYear = year % 4 == 0 and year % 400 ~= 0
     if month > 0 and month <= 12 then
-        local longMounth = month % 2 ~= 0
-        if day > 0 and ((month == 2 and ((leapYear and day <= 29) or (not leapYear and day <= 28))) or (month ~= 2 and ((longMounth and day <= 31) or (not longMounth and day <= 30)))) then
+        local longMonth = month % 2 ~= 0
+        if day > 0 and ((month == 2 and ((leapYear and day <= 29) or (not leapYear and day <= 28))) or (month ~= 2 and ((longMonth and day <= 31) or (not longMonth and day <= 30)))) then
             return true
         end
         return "day"
