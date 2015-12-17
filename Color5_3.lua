f1e302bd79845a8b6727bcd1d69be24eda556716
Squalleze <vinnicius_giroto@outlook.com>
2015-12-16 23:14:26 -0200

    Create Color5_3.lua

---
+++ Color5_3.lua
@@ -0,0 +1,11 @@
+HEX2ARGB = function(hex)
+    if type(hex) == "string" then
+        hex = tonumber(hex, 16)
+    end
+
+    return {hex >> 24 & 255, hex >> 16 & 255, hex >> 8 & 255, hex & 255}
+end
+
+ARGB2HEX = function(alpha, red, green, blue)
+    return (blue or 0) | (green or 0) << 8 | (red or 0) << 16 | (alpha or 0) << 24
+end
