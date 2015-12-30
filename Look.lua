0b04ddedd93184ae50defee01ba6c482c742e860
Squalleze <vinnicius_giroto@outlook.com>
2015-12-30 17:41:15 -0200

    Create Look.lua

---
+++ Look.lua
@@ -0,0 +1,15 @@
+-- Created by Squalleze
+look = function(l)
+    local fur, acessories = l:match("(%d)(.*)")
+    local out = {tonumber(fur)}
+
+    for a, b in acessories:gmatch("[;,](%d+)([_+%x]*)") do
+        local add = {id = tonumber(a), colors = {}}
+        for c in b:gmatch("[_+](%x+)") do
+            table.insert(add.colors, c)
+        end
+        table.insert(out, add)
+    end
+
+    return out
+end
