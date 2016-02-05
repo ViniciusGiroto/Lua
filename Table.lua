8f3b61c2bb9e520cd3d101e322000e7fde4c69cb
Squalleze <vinnicius_giroto@outlook.com>
2016-02-05 04:06:50 -0200

    Update Table.lua

--- Table.lua
+++ Table.lua
@@ -3,7 +3,7 @@ table.dump = function(list, o)
   o = (o or 0) > 0 and o or 0
   local out = {}
   for k, v in pairs(list) do
-    table.insert(out, ("%s%s = %s"):format(("\t"):rep(o + 1), type(k) ~= "number" and tostring(k) or ("[%s]"):format(k), type(v) == "table" and table.dump(v, o + 1) or ((type(v) == "number" or type(v) == "boolean") and tostring(v) or ("\"%s\""):format(tostring(v):gsub("\"", "\\\"")))))
+    table.insert(out, ("%s[%s] = %s"):format(("\t"):rep(o + 1), type(k) ~= "number" and ("\"%s\""):format(k) or k, type(v) == "table" and table.dump(v, o + 1) or ((type(v) == "number" or type(v) == "boolean") and tostring(v) or ("\"%s\""):format(tostring(v):gsub("\"", "\\\"")))))
   end
 
   local rt = ("{\n%s\n%s}"):format(table.concat(out, ",\n"), ("\t"):rep(o))
